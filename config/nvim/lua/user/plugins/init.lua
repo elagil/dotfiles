cdfaf8400c77f8f6d03cebb6f56ad881464b356d
elagil <elagil@takanome.de>
2022-10-31 12:29:52 +0100

    fix dependencies

--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -34,7 +34,7 @@ return packer.startup(function(use)
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.0",
 		requires = {
-			{ "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
+			{ "nvim-lua/plenary.nvim" },
 		},
 	})
 
