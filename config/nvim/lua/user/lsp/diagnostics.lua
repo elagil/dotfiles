57cf2c643098c7ed8da0004518d9eb4308ce805f
Adrian Figueroa <adrian.figueroa@metirionic.com>
2022-11-02 09:00:21 +0100

    fix: sign column

--- config/nvim/lua/user/lsp/diagnostics.lua
+++ config/nvim/lua/user/lsp/diagnostics.lua
@@ -1,28 +1,28 @@
 -- Set up VIM diagnostics
-local symbols = { Error = "", Warn = "", Hint = "", Info = "" }
--- local empty = { Error = "", Warn = "", Hint = "", Info = "" }
+M = {}
+M.symbols = { Error = "", Warn = "", Hint = "", Info = "", Other = "" }
+empty = { Error = "", Warn = "", Hint = "", Info = "" }
 
-local signs = symbols -- Can be `symbols` or `empty`
-
-for type, label in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = label, texthl = hl, numhl = hl })
+for type, label in pairs(empty) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = label, texthl = hl, numhl = hl })
 end
 
 local config = {
-	virtual_text = false,
-	signs = signs,
-	update_in_insert = false,
-	underline = false,
-	severity_sort = true,
-	float = {
-		focusable = false,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
+    virtual_text = false,
+    signs = true,
+    update_in_insert = false,
+    underline = false,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
 }
 
 vim.diagnostic.config(config)
+return M
